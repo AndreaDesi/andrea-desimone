6ac2011be2ef7ebd0e034624cc695dc7cd98cd0d
AndreaDesi <andreadesimo1998@gmail.com>
2022-11-02 19:12:52 +0100

    first commit

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Header!</h1>
+  <h1>Footer!</h1>
 </template>
 
 <script>
--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,67 @@
 <template>
-  <h1>Footer!</h1>
+  <nav>
+    <div class="menu-nav-container">
+      <ul id="menu-nav" class="menu">
+        <li
+          id="menu-item-77"
+          class="catcus menu-item-77"
+        >
+          <nuxt-link to="/">✨︎</nuxt-link>
+        </li>
+        <li
+          id="menu-item-78"
+          class="menu-item-78"
+        >
+        <nuxt-link to="/projects">Projects</nuxt-link>
+        </li>
+        <li
+          id="menu-item-79"
+          class="menu-item-79"
+        >
+        <nuxt-link to="/about">About</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+nav {
+  height: 8.5vh;
+  align-items: center;
+  display: grid;
+  align-content: stretch;
+}
+
+.menu-nav-container {
+}
+
+#menu-nav {
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-gap: 1rem;
+  margin: 0px 0px 0px 0px;
+  align-items: center;
+}
+
+#menu-item-77 {
+  grid-column: 1;
+  font-size: calc(1.1em * 1.2);
+}
+
+#menu-item-78 {
+  grid-column: 9;
+  text-align: right;
+  text-transform: uppercase;
+}
+
+#menu-item-79 {
+  grid-column: 10;
+  text-align: right;
+  text-transform: uppercase;
+}
+</style>
