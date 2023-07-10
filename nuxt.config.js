c913c74ce3c8e21457a4f0c3770566cd1ec4b51f
Luca Argentieri <Lucaargentieri-98@hotmail.it>
2023-07-10 14:05:26 +0200

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,134 +4,123 @@ export default {
   target: "static",
   ssr: false,
   loading: false,
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Andrea De Simone",
     htmlAttrs: {
-      lang: "en",
+      lang: "it",
     },
+    titleTemplate: "%s - " + "Andrea De Simone",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
+      {
+        rel: "shortcut icon",
+        href: "/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      {
+        rel: "mask-icon",
+        href: "/safari-pinned-tab.svg",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        name: "description",
+        content:
+          "Ciao! I’m Andrea De Simone, a communication designer based in Italy and I’m currently attending the last year of Master degree in Communication Design at Politecnico di Milano.",
+      },
+      {
+        name: "image",
+        content: "",
+      },
+      // Twitter
+      {
+        hid: "twitter:card",
+        name: "twitter:card",
+        content: "summary_large_image",
+      },
+      {
+        hid: "twitter:site",
+        name: "twitter:site",
+        content: "Andrea De Simone",
+      },
+      {
+        hid: "twitter:url",
+        name: "twitter:url",
+        content: "https://andreadesimone.info/",
+      },
+      {
+        hid: "twitter:title",
+        name: "twitter:title",
+        content: "Andrea De Simone",
+      },
+      {
+        hid: "twitter:description",
+        name: "twitter:description",
+        content:
+          "Ciao! I’m Andrea De Simone, a communication designer based in Italy and I’m currently attending the last year of Master degree in Communication Design at Politecnico di Milano.",
+      },
+      {
+        hid: "twitter:image",
+        name: "twitter:image",
+        content: "",
+      },
+
+      // Open Graph
+      {
+        hid: "og:site_name",
+        property: "og:site_name",
+        content: "Andrea De Simone",
+      },
+      { hid: "og:type", property: "og:type", content: "website" },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://andreadesimone.info/",
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "Andrea De Simone",
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content:
+          "Ciao! I’m Andrea De Simone, a communication designer based in Italy and I’m currently attending the last year of Master degree in Communication Design at Politecnico di Milano.Un'azienda vinicola che ricerca nella produzione di franciacorta l'espressione autentica del territorio.",
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "",
+      },
+      {
+        hid: "og:image:secure_url",
+        property: "og:image:secure_url",
+        content: "",
+      },
+      {
+        hid: "og:image:alt",
+        property: "og:image:alt",
+        content: "Andrea De Simone Logo",
+      },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  head() {
-    return {
-      title: "Home",
-      htmlAttrs: {
-        lang: "it",
-      },
-      titleTemplate: "%s - " + "Andrea De Simone",
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "format-detection", content: "telephone=no" },
-        {
-          rel: "shortcut icon",
-          href: "/favicon.ico",
-        },
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "32x32",
-          href: "/favicon-32x32.png",
-        },
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
-          href: "/favicon-16x16.png",
-        },
-        {
-          rel: "mask-icon",
-          href: "/safari-pinned-tab.svg",
-        },
-        {
-          rel: "apple-touch-icon",
-          sizes: "180x180",
-          href: "/apple-touch-icon.png",
-        },
-        {
-          name: "description",
-          content:
-            "Ciao! I’m Andrea De Simone, a communication designer based in Italy and I’m currently attending the last year of Master degree in Communication Design at Politecnico di Milano.",
-        },
-        {
-          name: "image",
-          content: "",
-        },
-        // Twitter
-        {
-          hid: "twitter:card",
-          name: "twitter:card",
-          content: "summary_large_image",
-        },
-        { hid: "twitter:site", name: "twitter:site", content: "Andrea De Simone" },
-        {
-          hid: "twitter:url",
-          name: "twitter:url",
-          content: "https://andreadesimone.info/",
-        },
-        {
-          hid: "twitter:title",
-          name: "twitter:title",
-          content: "Andrea De Simone",
-        },
-        {
-          hid: "twitter:description",
-          name: "twitter:description",
-          content:
-            "Ciao! I’m Andrea De Simone, a communication designer based in Italy and I’m currently attending the last year of Master degree in Communication Design at Politecnico di Milano.",
-        },
-        {
-          hid: "twitter:image",
-          name: "twitter:image",
-          content: "",
-        },
-
-        // Open Graph
-        {
-          hid: "og:site_name",
-          property: "og:site_name",
-          content: "Andrea De Simone",
-        },
-        { hid: "og:type", property: "og:type", content: "website" },
-        {
-          hid: "og:url",
-          property: "og:url",
-          content: "https://andreadesimone.info/",
-        },
-        {
-          hid: "og:title",
-          property: "og:title",
-          content: "Andrea De Simone",
-        },
-        {
-          hid: "og:description",
-          property: "og:description",
-          content:
-            "Ciao! I’m Andrea De Simone, a communication designer based in Italy and I’m currently attending the last year of Master degree in Communication Design at Politecnico di Milano.Un'azienda vinicola che ricerca nella produzione di franciacorta l'espressione autentica del territorio.",
-        },
-        {
-          hid: "og:image",
-          property: "og:image",
-          content: "",
-        },
-        {
-          hid: "og:image:secure_url",
-          property: "og:image:secure_url",
-          content: "",
-        },
-        {
-          hid: "og:image:alt",
-          property: "og:image:alt",
-          content: "Andrea De Simone Logo",
-        },
-      ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    };
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
